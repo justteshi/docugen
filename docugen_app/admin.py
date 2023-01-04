4e827025c5a1f4bb781bea0bce62ad85021f50f1
Teodor Hristov <justteshi@gmail.com>
2023-01-04 11:11:37 +0200

    Add/ Add Proektants and multiple select in form

--- docugen_app/admin.py
+++ docugen_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Proektant
 
 # Register your models here.
+@admin.register(Proektant)
+class ProetantAdmin(admin.ModelAdmin):
+    list_display = ('name',)
--- docugen_app/views.py
+++ docugen_app/views.py
@@ -3,6 +3,7 @@ from docx import Document
 from docx.shared import Pt
 from datetime import date
 from django.shortcuts import redirect
+from .models import Proektant
 # Create your views here.
 
 
@@ -15,11 +16,12 @@ def index(request):
 def kompleksen_doklad(request):
     if request.method == 'GET':
         # form = KompleksenDokladForm()
-
+        proektants = Proektant.objects.all()
         template = 'pages/kompleksen.html'
 
         context = {
             # 'form': form
+            'proektants': proektants
         }
 
         return render(request, template, context)
@@ -27,13 +29,19 @@ def kompleksen_doklad(request):
     if request.method == 'POST':
         form_data = request.POST
         document = Document()
-        title = document.add_paragraph().add_run(form_data['docnum'])
+        title = document.add_paragraph().add_run('АРЕА ПРОЕКТ ЕООД, гр. Разград, ул. „Кирил и Методий“ №3, ет2, офис10')
         title.font.size = Pt(14)
-
-        document.add_paragraph(form_data['vazlozhitel'])
-        document.add_paragraph(form_data['build'])
-        document.add_paragraph(form_data['location'])
-
+        title.bold = True
+        document.add_paragraph('тел/факс: 084 620000; 0893 316563, e-mal: area_proekt@abv.bg')
+        doc_num = document.add_paragraph().add_run('Изх. №  {docnum} / {date} '.format(docnum=form_data['docnum'], date=date.today()))
+        doc_num.font.size = Pt(11)
+        vazlozhitel_title = document.add_paragraph().add_run('До ВЪЗЛОЖИТЕЛЯ :') 
+        vazlozhitel_title.font.size = Pt(14)
+        vazlozhitel_title.bold = True
+        vazlozhitel = document.add_paragraph().add_run(form_data['build'])
+        vazlozhitel.font.size = Pt(14)
+        vazlozhitel.bold = True
+        
         document.save('demo.docx')
 
         return redirect('/')
